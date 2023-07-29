fa692cdddc395d5a4fb41a169395ec8a78581757
DickLan <pokemaster556610@gmail.com>
2023-07-29 15:06:37 +0800

    feat: modify seeders

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -3,6 +3,9 @@ const Restaurant = require('../restaurant')
 // 取得資料庫連線狀態
 const db = require('../../config/mongoose')
 const bcrypt = require('bcryptjs')
+const resJson = require('../../restaurant.json')
+const restaurantsFromJson = resJson.results
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -15,25 +18,45 @@ const SEED_USER = {
 
 // 連線成功 另外 執行callback後就會解除監聽器
 db.once('open', () => {
-  bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER.password, salt))
-    .then(hash => User.create({
-      name: SEED_USER.name,
-      email: SEED_USER.email,
-      password: hash
-    }))
+  // console.log(restaurantsFromJson)
+  User.findOne({ email: SEED_USER.email })
     .then(user => {
-      const userId = user._id
-      // promise.all 在發出10筆'請求'後，會確保'完成'回收10筆回應後，才進入下一個then
-      return Promise.all(Array.from(
-        { length: 10 },
-        // 第一個參數是可迭代物件 這裡用不到所以用＿
-        (_, i) => Restaurant.create({ name: `name-${i}`, userId })
-      ))
+      if (user) {
+        console.log('seed existed.')
+        process.exit()
+      }
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(SEED_USER.password, salt))
+        .then(hash => User.create({
+          name: SEED_USER.name,
+          email: SEED_USER.email,
+          password: hash
+        }))
+        .then(user => {
+          const userId = user._id
+          // promise.all 在發出10筆'請求'後，會確保'完成'回收10筆回應後，才進入下一個then
+          return Promise.all(Array.from(
+            { length: 8 },
+            // 第一個參數是可迭代物件 這裡用不到所以用＿
+            (_, i) => Restaurant.create({
+              name: `${restaurantsFromJson[i].name}`,
+              name_en: `${restaurantsFromJson[i].name_en}`,
+              category: `${restaurantsFromJson[i].category}`,
+              image: `${restaurantsFromJson[i].image}`,
+              location: `${restaurantsFromJson[i].location}`,
+              rating: `${restaurantsFromJson[i].rating}`,
+              description: `${restaurantsFromJson[i].description}`,
+
+              userId
+            })
+          ))
+        })
+        .then(() => {
+          console.log('created!')
+          process.exit()
+        })
     })
-    .then(() => {
-      console.log('done')
-      process.exit()
-    })
+
+
 })
